6bd7b338dd481a20f1c4546addde1e0fba6abb2a
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2020-11-23 15:03:28 +0400

    Refactoring

---
+++ Assets/Group1/Scripts/Enemy/Wandering.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Wandering : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _wanderRange;
+
+    private Vector3 _target;
+
+    private Vector3 _randomTarget { get => Random.insideUnitCircle * _wanderRange; }
+
+    private void Start()
+    {
+        _target = _randomTarget;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _moveSpeed * Time.deltaTime);
+        
+        if (Vector3.Distance(transform.position, _target) <= 0.01f)
+            _target = _randomTarget;
+    }
+}
---
+++ Assets/Group1/Scripts/GameOverChecker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverChecker : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Enemy[] _enemies;
+
+    private int _counterKilledEnemies;
+
+    private void OnEnable()
+    {
+        foreach (var enemy in _enemies)
+        {
+            enemy.Died += OnEnemyDied;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var enemy in _enemies)
+        {
+            enemy.Died -= OnEnemyDied;
+        }
+    }
+
+    private void OnEnemyDied()
+    {
+        _counterKilledEnemies++;
+
+        if (_counterKilledEnemies == _enemies.Length)
+        {
+            FinishGame();
+        }
+    }
+
+    private void FinishGame()
+    {
+        _spriteRenderer.enabled = true;
+    }
+}
